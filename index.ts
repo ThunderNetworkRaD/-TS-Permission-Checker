f2cf7e662ce36cdbfb0b5209e0a86cfaaaa09302
KillerBossOriginal <killerbossoriginal@outlook.it>
2023-06-04 15:15:34 +0200

    init

---
+++ index.ts
@@ -0,0 +1,19 @@
+/**
+ * Check if a permission is granted
+ * @param {Array<String>} perm  - Is the permission that the user have
+ * @param {Array<String>} reqPerm - Is the required permissions
+ * @returns {Boolean}
+ */
+function checkList(perm: string[], reqPerm: string[]) {
+    let answer = false, trueL = 0;
+
+    for (const p of perm) {
+        
+    }
+
+    return answer;
+}
+
+export default {
+    checkList,
+}
